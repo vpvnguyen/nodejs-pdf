e082c3f207ccc37deafcd7c5c131433d658ebd16
vpvnguyen <vpvnguyen@gmail.com>
2021-03-19 15:10:01 -0700

    create new methods to create and store PDFs

--- pdfkit-react-express/controllers/Pdf.controller.js
+++ pdfkit-react-express/controllers/Pdf.controller.js
@@ -8,7 +8,7 @@ class PdfController {
     res.writeHead(200, {
       "Content-Type": "application/pdf",
       "Access-Control-Allow-Origin": "*",
-      "Content-Disposition": "attachment; filename=test.pdf",
+      "Content-Disposition": "attachment; filename=getPdf.pdf",
     });
 
     // Pipe its output somewhere, like to a file or HTTP response
@@ -32,7 +32,7 @@ class PdfController {
     res.writeHead(200, {
       "Content-Type": "application/pdf",
       "Access-Control-Allow-Origin": "*",
-      "Content-Disposition": "attachment; filename=test.pdf",
+      "Content-Disposition": "attachment; filename=createPdf.pdf",
     });
 
     // Pipe its output somewhere, like to a file or HTTP response
@@ -41,6 +41,24 @@ class PdfController {
 
     // Embed a font, set the font size, and render some text
     doc.font("Times-Roman").fontSize(25).text(text, 100, 100);
+
+    // Finalize PDF file
+    doc.end();
+  };
+
+  static storePdf = (text, res) => {
+    // Create a document
+    const doc = new PDFDocument();
+
+    // Pipe its output somewhere, like to a file or HTTP response
+    // See below for browser usage
+    doc.pipe(fs.createWriteStream(`output-${new Date()}.pdf`));
+
+    // Embed a font, set the font size, and render some text
+    doc
+      .font("Times-Roman")
+      .fontSize(25)
+      .text("Some text with an embedded font!", 100, 100);
 
     // Finalize PDF file
     doc.end();
