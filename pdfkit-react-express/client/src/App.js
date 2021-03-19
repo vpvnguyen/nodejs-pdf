ed8f3cb2e625f45c00978d654391c02ab3966bab
vpvnguyen <vpvnguyen@gmail.com>
2021-03-19 15:09:42 -0700

    create new handlers for creating and storing PDFs; renamed generatePdf to getPdf to align with server route

--- pdfkit-react-express/client/src/App.js
+++ pdfkit-react-express/client/src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const generatePdf = async (event) => {
+  const getPdf = async (event) => {
+    console.log("getPdf");
+
     event.preventDefault();
 
-    const response = await axios.get("http://localhost:5000/pdf", {
-      responseType: "blob",
-    });
+    const response = await axios.get("http://localhost:5000/get-pdf");
 
     const file = new File([response.data], "test.pdf", {
       type: "application/pdf",
@@ -16,10 +16,39 @@ function App() {
     console.log(file);
   };
 
+  const createPdf = async (event) => {
+    console.log("createPdf");
+
+    event.preventDefault();
+
+    const response = await axios.post("http://localhost:5000/create-pdf", {
+      responseType: "blob",
+    });
+    const file = new File([response.data], "test.pdf", {
+      type: "application/pdf",
+    });
+    console.log(file);
+  };
+
+  const storePdf = async (event) => {
+    console.log("storePdf");
+
+    event.preventDefault();
+
+    const response = await axios.post(
+      "http://localhost:5000/store-pdf",
+      "store pdf text"
+    );
+
+    console.log(response);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={generatePdf}>Generate PDF</button>
+        <button onClick={getPdf}>Get PDF</button>
+        <button onClick={createPdf}>Create PDF</button>
+        <button onClick={storePdf}>Store PDF</button>
       </header>
     </div>
   );
