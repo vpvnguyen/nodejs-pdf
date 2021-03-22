ef9f2ed6c4046ab993f1ca244ba16179fe09c285
Vincent Nguyen <vpvnguyen@gmail.com>
2021-03-21 22:18:01 -0700

    removed comments from PDFKIT library

--- pdfkit-react-express/controllers/Pdf.controller.js
+++ pdfkit-react-express/controllers/Pdf.controller.js
@@ -2,7 +2,6 @@ const PDFDocument = require("pdfkit");
 const fs = require("fs");
 class PdfController {
   static getPdf = (res) => {
-    // Create a document
     const doc = new PDFDocument();
 
     res.writeHead(200, {
@@ -11,22 +10,17 @@ class PdfController {
       "Content-Disposition": "attachment; filename=getPdf.pdf",
     });
 
-    // Pipe its output somewhere, like to a file or HTTP response
-    // See below for browser usage
     doc.pipe(res);
 
-    // Embed a font, set the font size, and render some text
     doc
       .font("Times-Roman")
       .fontSize(25)
       .text("Some text with an embedded font!", 100, 100);
 
-    // Finalize PDF file
     doc.end();
   };
 
   static createPdf = (text, res) => {
-    // Create a document
     const doc = new PDFDocument();
 
     res.writeHead(200, {
@@ -35,35 +29,26 @@ class PdfController {
       "Content-Disposition": "attachment; filename=createPdf.pdf",
     });
 
-    // Pipe its output somewhere, like to a file or HTTP response
-    // See below for browser usage
     doc.pipe(res);
 
-    // Embed a font, set the font size, and render some text
     doc.font("Times-Roman").fontSize(25).text(text, 100, 100);
 
-    // Finalize PDF file
     doc.end();
   };
 
   static storePdf = (text) => {
-    // Create a document
     const doc = new PDFDocument();
 
     const pdfOutputDirectory = "./pdf-output";
 
     if (!fs.existsSync(pdfOutputDirectory)) fs.mkdirSync(pdfOutputDirectory);
 
-    // Pipe its output somewhere, like to a file or HTTP response
-    // See below for browser usage
     doc.pipe(
       fs.createWriteStream(`${pdfOutputDirectory}/output-${new Date()}.pdf`)
     );
 
-    // Embed a font, set the font size, and render some text
     doc.font("Times-Roman").fontSize(25).text(text, 100, 100);
 
-    // Finalize PDF file
     doc.end();
   };
 }
